af7781f85200a9d3dfbaea5fb23b9bf92c5a4bb8
xyw1042971947 <1042971947@qq.com>
2020-08-15 19:53:07 +0800

    切面日志处理

---
+++ mall-admin/src/main/java/com/ywxiang/mall/bo/WebLog.java
@@ -0,0 +1,62 @@
+package com.ywxiang.mall.bo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 日志类
+ * @author ywxiang
+ * @date 2020/8/15 下午7:18
+ */
+@Getter
+@Setter
+public class WebLog {
+    /**
+     * 操作描述
+     */
+    private String description;
+
+    /**
+     * 操作用户
+     */
+    private String username;
+
+    /**
+     * 操作时间
+     */
+    private Long startTime;
+
+    /**
+     * 消耗时间
+     */
+    private Integer spendTime;
+
+    /**
+     * 根路径
+     */
+    private String basePath;
+
+    /**
+     * URI
+     */
+    private String uri;
+
+    /**
+     * URL
+     */
+    private String url;
+
+    /**
+     * 请求类型
+     */
+    private String method;
+
+    /**
+     * IP地址
+     */
+    private String ip;
+
+    private Object parameter;
+
+    private Object result;
+}
