19c19c74d2d23fa9462ea7651e2bb708ad237efe
xyw1042971947 <1042971947@qq.com>
2020-08-30 14:00:18 +0800

    /product/create

--- mall-admin/src/main/java/com/ywxiang/mall/controller/PmsProductController.java
+++ mall-admin/src/main/java/com/ywxiang/mall/controller/PmsProductController.java
@@ -2,16 +2,14 @@ package com.ywxiang.mall.controller;
 
 import com.ywxiang.mall.api.CommonPage;
 import com.ywxiang.mall.api.CommonResult;
+import com.ywxiang.mall.dto.PmsProductParam;
 import com.ywxiang.mall.dto.PmsProductQueryParam;
 import com.ywxiang.mall.model.PmsProduct;
 import com.ywxiang.mall.service.PmsProductService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,5 +31,15 @@ public class PmsProductController {
                                 @RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum) {
         List<PmsProduct> productList = productService.list(productQueryParam, pageSize, pageNum);
         return CommonResult.success(CommonPage.restPage(productList));
+    }
+
+    @ApiOperation("创建商品")
+    @PostMapping("/create")
+    public CommonResult create(@RequestBody PmsProductParam productParam) {
+        int count = productService.create(productParam);
+        if (count > 0 ) {
+            return CommonResult.success(count);
+        }
+        return CommonResult.failed();
     }
 }
--- mall-admin/src/main/java/com/ywxiang/mall/service/impl/PmsProductServiceImpl.java
+++ mall-admin/src/main/java/com/ywxiang/mall/service/impl/PmsProductServiceImpl.java
@@ -1,12 +1,13 @@
 package com.ywxiang.mall.service.impl;
 
 import com.github.pagehelper.PageHelper;
-import com.ywxiang.mall.dao.PmsMemberPriceDao;
+import com.ywxiang.mall.dao.*;
 import com.ywxiang.mall.dto.PmsProductParam;
 import com.ywxiang.mall.dto.PmsProductQueryParam;
 import com.ywxiang.mall.mapper.PmsProductMapper;
 import com.ywxiang.mall.model.PmsProduct;
 import com.ywxiang.mall.model.PmsProductExample;
+import com.ywxiang.mall.model.PmsSkuStock;
 import com.ywxiang.mall.service.PmsProductService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,8 @@ import org.springframework.util.StringUtils;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -32,6 +35,18 @@ public class PmsProductServiceImpl implements PmsProductService {
     PmsProductMapper productMapper;
     @Autowired
     PmsMemberPriceDao memberPriceDao;
+    @Autowired
+    PmsProductLadderDao productLadderDao;
+    @Autowired
+    PmsProductFullReductionDao productFullReductionDao;
+    @Autowired
+    PmsSkuStockDao skuStockDao;
+    @Autowired
+    PmsProductAttributeValueDao productAttributeValueDao;
+    @Autowired
+    CmsSubjectProductRelationDao subjectProductRelationDao;
+    @Autowired
+    CmsPreferenceAreaProductRelationDao preferenceAreaProductRelationDao;
 
     @Override
     public List<PmsProduct> list(PmsProductQueryParam productQueryParam, Integer pageSize, Integer pageNum) {
@@ -64,11 +79,36 @@ public class PmsProductServiceImpl implements PmsProductService {
 
     @Override
     public int create(PmsProductParam productParam) {
-        return 0;
+        int count;
+        // 创建商品
+        PmsProduct product = productParam;
+        product.setId(null);
+        productMapper.insertSelective(product);
+        // 根据促销类型设置价格：会员价格、阶梯价格、满减价格
+        Long productId = product.getId();
+        // 会员价格
+        relationAndInsertList(memberPriceDao, productParam.getMemberPriceList(), productId);
+        // 阶梯价格
+        relationAndInsertList(productLadderDao, productParam.getProductLadderList(), productId);
+        // 满减价格
+        relationAndInsertList(productFullReductionDao, productParam.getProductFullReductionList(), productId);
+        // 处理SKu编码
+        handleSkuStockCode(productParam.getSkuStockList(), productId);
+        // 添加Sku库存信息
+        relationAndInsertList(skuStockDao, productParam.getSkuStockList(), productId);
+        // 添加商品参数，添加自定义商品规格
+        relationAndInsertList(productAttributeValueDao, productParam.getProductAttributeValueList(), productId);
+        // 关联专题
+        relationAndInsertList(subjectProductRelationDao, productParam.getSubjectProductRelationList(), productId);
+        // 关联优选
+        relationAndInsertList(preferenceAreaProductRelationDao, productParam.getPrefrenceAreaProductRelationList(), productId);
+        count = 1;
+        return count;
     }
 
     /**
      * 建立和插入关系操作
+     *
      * @param dao
      * @param dateList
      * @param productId
@@ -89,6 +129,31 @@ public class PmsProductServiceImpl implements PmsProductService {
         } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
             log.warn("创建产品出错:{}", e.getMessage());
             throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    /**
+     * 处理Sku编码
+     * @param skuStockList
+     * @param productId
+     */
+    private void handleSkuStockCode(List<PmsSkuStock> skuStockList, Long productId) {
+        if (CollectionUtils.isEmpty(skuStockList)) {
+            return;
+        }
+        for (int i = 0; i < skuStockList.size(); i++) {
+            PmsSkuStock skuStock = skuStockList.get(i);
+            if (StringUtils.isEmpty(skuStock.getSkuCode())) {
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+                StringBuilder sb = new StringBuilder();
+                // 日期
+                sb.append(dateFormat.format(new Date()));
+                // 思维商品id
+                sb.append(String.format("%04d", productId));
+                // 三位索引Id
+                sb.append(String.format("%03d", i + 1));
+                skuStock.setSkuCode(sb.toString());
+            }
         }
     }
 }
