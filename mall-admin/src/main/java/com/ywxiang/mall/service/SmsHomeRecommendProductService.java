f4a5e2c4a9a673662d99a74fd37d9a0f712c7412
xyw1042971947 <1042971947@qq.com>
2020-09-05 15:56:07 +0800

    首页人气推荐管理API

---
+++ mall-admin/src/main/java/com/ywxiang/mall/service/SmsHomeRecommendProductService.java
@@ -0,0 +1,8 @@
+package com.ywxiang.mall.service;
+
+/**
+ * @author ywxiang
+ * @date 2020/9/5 下午3:49
+ */
+public interface SmsHomeRecommendProductService {
+}
---
+++ mall-admin/src/main/java/com/ywxiang/mall/service/impl/SmsHomeRecommendProductServiceImpl.java
@@ -0,0 +1,8 @@
+package com.ywxiang.mall.service.impl;
+
+/**
+ * @author ywxiang
+ * @date 2020/9/5 下午3:52
+ */
+public class SmsHomeRecommendProductServiceImpl {
+}
