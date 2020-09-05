a48080bd8aecf7364902c72bb8023a684360dc8f
xyw1042971947 <1042971947@qq.com>
2020-09-05 15:42:38 +0800

    首页专题管理API

---
+++ mall-admin/src/main/java/com/ywxiang/mall/controller/SmsHomeRecommendSubjectController.java
@@ -0,0 +1,8 @@
+package com.ywxiang.mall.controller;
+
+/**
+ * @author ywxiang
+ * @date 2020/9/5 下午3:36
+ */
+public class SmsHomeRecommendSubjectController {
+}
---
+++ mall-admin/src/main/java/com/ywxiang/mall/service/SmsHomeRecommendSubjectService.java
@@ -0,0 +1,8 @@
+package com.ywxiang.mall.service;
+
+/**
+ * @author ywxiang
+ * @date 2020/9/5 下午3:19
+ */
+public interface SmsHomeRecommendSubjectService {
+}
