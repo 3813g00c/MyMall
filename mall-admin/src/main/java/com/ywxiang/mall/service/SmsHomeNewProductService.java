12690678d67768137c3a483592a28dd47e027ef7
xyw1042971947 <1042971947@qq.com>
2020-09-05 16:10:51 +0800

    首页新品推荐管理API

---
+++ mall-admin/src/main/java/com/ywxiang/mall/service/SmsHomeNewProductService.java
@@ -0,0 +1,8 @@
+package com.ywxiang.mall.service;
+
+/**
+ * @author ywxiang
+ * @date 2020/9/5 下午4:04
+ */
+public interface SmsHomeNewProductService {
+}
---
+++ mall-admin/src/main/java/com/ywxiang/mall/service/impl/SmsHomeNewProductServiceImpl.java
@@ -0,0 +1,8 @@
+package com.ywxiang.mall.service.impl;
+
+/**
+ * @author ywxiang
+ * @date 2020/9/5 下午4:06
+ */
+public class SmsHomeNewProductService {
+}
