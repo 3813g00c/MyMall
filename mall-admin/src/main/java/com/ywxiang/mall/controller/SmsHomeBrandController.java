4d0db2204392dca9e397f061a68511a784c0a841
xyw1042971947 <1042971947@qq.com>
2020-09-05 16:22:34 +0800

    首页品牌推荐管理API

---
+++ mall-admin/src/main/java/com/ywxiang/mall/controller/SmsHomeBrandController.java
@@ -0,0 +1,8 @@
+package com.ywxiang.mall.controller;
+
+/**
+ * @author ywxiang
+ * @date 2020/9/5 下午4:21
+ */
+public class SmsHomeBrandController {
+}
---
+++ mall-admin/src/main/java/com/ywxiang/mall/service/SmsHomeBrandService.java
@@ -0,0 +1,8 @@
+package com.ywxiang.mall.service;
+
+/**
+ * @author ywxiang
+ * @date 2020/9/5 下午4:12
+ */
+public interface SmsHomeBrandService {
+}
