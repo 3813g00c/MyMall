4f6f9507ef94a62396ba0b91fa0c1925bce4e45c
xyw1042971947 <1042971947@qq.com>
2020-09-07 20:51:04 +0800

    首页广告管理API

---
+++ mall-admin/src/main/java/com/ywxiang/mall/service/SmsHomeAdvertiseService.java
@@ -0,0 +1,8 @@
+package com.ywxiang.mall.service;
+
+/**
+ * @author ywxiang
+ * @date 2020/9/7 下午8:39
+ */
+public interface SmsHomeAdvertiseService {
+}
---
+++ mall-admin/src/main/java/com/ywxiang/mall/service/impl/SmsHomeAdvertiseServiceImpl.java
@@ -0,0 +1,8 @@
+package com.ywxiang.mall.service.impl;
+
+/**
+ * @author ywxiang
+ * @date 2020/9/7 下午8:41
+ */
+public class SmsHomeAdvertiseServiceImpl {
+}
